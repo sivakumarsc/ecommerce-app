23689e5d45e03f9adfb798a561d4d044bca92e1f
sivakumar <sivakumarsc.me@gmail.com>
2021-08-28 20:48:57 +0530

    add header and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer  from './components/Footer';
 
 const App = () => {
-  return (<h1>
-    Hello World!!
-  </h1>)
+  return (<>
+    <Header />
+    <main>
+      <Container>
+        <h1>Hello World!!</h1>
+      </Container>
+    </main>
+    <Footer />
+  </>)
 }
 
 export default App;
