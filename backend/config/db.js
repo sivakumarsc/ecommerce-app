a13c6689384dc5ea49af107fbd75515916a03523
sivakumar <sivakumarsc.me@gmail.com>
2021-10-30 18:03:47 +0530

    add data models

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,9 +3,9 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log(`MongoDB Connected: ${conn.connection.host}`.yellow.underline);
   } catch (error) {
-    console.log(`Error while connecting to MongoDB: ${error.message}`);
+    console.log(`Error while connecting to MongoDB: ${error.message}`.red.bold.underline);
     process.exit(1);
   }
 };
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import colors from 'colors';
 import products from './data/products.js';
 import connectDB from './config/db.js';
 
@@ -24,4 +25,4 @@ app.get('/api/products/:id', (req, res) => {
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${port}`));
+app.listen(port, console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${port}`.green.bold));
